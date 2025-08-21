0ecafd9cca7ac21b2b97f986ff4b439c5281ca53
Yonatan Karp-Rudin <yonvata@gmail.com>
2025-08-21 09:41:49 +0200

    Add initial Gradle setup and configuration files

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "coffee-machine-simulator"
+
+include(
+    "coffee-machine-simulator-domain",
+    "coffee-machine-simulator-application",
+    "coffee-machine-simulator-adapters"
+)
