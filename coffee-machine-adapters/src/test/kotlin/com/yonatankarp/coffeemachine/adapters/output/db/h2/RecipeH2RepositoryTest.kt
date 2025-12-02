9f755a10a1f9568452087c6f7fce7be0bdabaf58
Yonatan Karp-Rudin <yonvata@gmail.com>
2025-12-02 09:50:29 +0100

    Make code happy after the upgrade

--- coffee-machine-adapters/src/test/kotlin/com/yonatankarp/coffeemachine/adapters/output/db/h2/RecipeH2RepositoryTest.kt
+++ coffee-machine-adapters/src/test/kotlin/com/yonatankarp/coffeemachine/adapters/output/db/h2/RecipeH2RepositoryTest.kt
@@ -4,10 +4,19 @@ import com.yonatankarp.coffeemachine.domain.recipe.Recipe
 import com.yonatankarp.coffeemachine.domain.recipe.RecipeFixture
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestConstructor
+import org.springframework.transaction.annotation.Transactional
 
-@DataJpaTest(showSql = true)
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE,
+    properties = [
+        "spring.main.web-application-type=none",
+        "spring.flyway.enabled=true",
+        "spring.jpa.hibernate.ddl-auto=none",
+    ],
+)
+@Transactional
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 class RecipeH2RepositoryTest(
     private val jpaRepository: RecipeH2JpaRepository,
